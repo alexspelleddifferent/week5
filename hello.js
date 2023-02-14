00b14ee35a25a9fdceefc13c3f34479f0fea11ed
Alex <alexkachan@protonmail.com>
2023-02-14 03:06:08 -0600

    first commit

---
+++ hello.js
@@ -0,0 +1,18 @@
+console.log('hello world')
+
+let Name='Alex'
+let health='healthy'
+
+function gpa(val) {
+    if (!(val < 0 || val >4)) {
+        return true
+    }
+
+    return false
+}
+
+console.log(Name)
+console.log(health)
+console.log(gpa(5))
+console.log(gpa(-5))
+console.log(gpa(3))
